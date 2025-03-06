f05165cc059a6889da02e5b90dc04a9e39c6b4ce
s33582 <s33582@pjwstk.edu.pl>
2025-03-06 09:41:57 +0100

    X

--- cwiczenie1/src/Main.java
+++ cwiczenie1/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Witaj swiecie");
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int a = 10;
+        int b = 12;
+        int c = 14;
     }
-
 }
